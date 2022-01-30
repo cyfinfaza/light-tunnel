c35068c2254c4122fc5ffac271f388e81212fab3
cyfinfaza <42043351+cyfinfaza@users.noreply.github.com>
2022-01-30 17:48:07 -0500

    mobile blurry fix

--- main.js
+++ main.js
@@ -41,6 +41,7 @@ function initCore() {
 	renderer = new THREE.WebGLRenderer({ antialias: true });
 	renderer.shadowMapEnabled = true;
 	renderer.setSize(windowWidth, windowHeight);
+	setPixelRatio(window.devicePixelRatio);
 	window.addEventListener("resize", _ => {
 		windowWidth = window.innerWidth;
 		windowHeight = window.innerHeight;
